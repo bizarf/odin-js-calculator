ca159ba40250a65920d9442fec1b42726c45a9da
bizarf <snesmaster40@googlemail.com>
2022-07-15 00:09:51 +0100

    Fixed some of the logic, and cleaned up the code by turning repeats into functions.

--- calculator.js
+++ calculator.js
@@ -1,8 +1,9 @@
 let displayValue = "";
 let chosenOperator = "";
-let firstValue = "";
 let secondValue = "";
 let totalValue = 0;
+let clearFlag = false;
+let clearFlagSecond = false;
 
 // Adds two numbers
 function addNum(a, b) {
@@ -41,181 +42,178 @@ function operate(operator, a, b) {
 // Zero button
 let zeroBtnPress = document.querySelector("#zeroBtn");
 zeroBtnPress.addEventListener('click', () => {
-    displayScreen.value += "0"
-    displayValue += "0"
+    if (displayValue === "") {
+        displayScreen.value = "0"
+        displayValue = "0"
+    } else if (displayValue != "0") {
+        displayScreen.value += "0"
+        displayValue += "0"
+    }
 })
 
 // One button
 let oneBtnPress = document.querySelector("#oneBtn");
 oneBtnPress.addEventListener('click', () => {
-    if (displayScreen.value <= "0") {
-        displayScreen.value = "";
-        displayScreen.value += "1"
-        displayValue += "1"
-    } else {
-        displayScreen.value += "1"
-        displayValue += "1"
-    }
+    numberBtn("1");
 })
 
 // Two button
 let twoBtnPress = document.querySelector("#twoBtn");
 twoBtnPress.addEventListener('click', () => {
-    if (displayScreen.value <= "0") {
-        displayScreen.value = "";
-        displayScreen.value += "2"
-        displayValue += "2"
-    } else {
-        displayScreen.value += "2"
-        displayValue += "2"
-    }
+    numberBtn("2");
 })
 
 // Three button
 let threeBtnPress = document.querySelector("#threeBtn");
 threeBtnPress.addEventListener('click', () => {
-    if (displayScreen.value <= "0") {
-        displayScreen.value = "";
-        displayScreen.value += "3"
-        displayValue += "3"
-    } else {
-        displayScreen.value += "3"
-        displayValue += "3"
-    }
+    numberBtn("3");
 })
 
 // Four button
 let fourBtnPress = document.querySelector("#fourBtn");
 fourBtnPress.addEventListener('click', () => {
-    if (displayScreen.value <= "0") {
-        displayScreen.value = "";
-        displayScreen.value += "4"
-        displayValue += "4"
-    } else {
-        displayScreen.value += "4"
-        displayValue += "4"
-    }
+    numberBtn("4");
 })
 
 // Five button
 let fiveBtnPress = document.querySelector("#fiveBtn");
 fiveBtnPress.addEventListener('click', () => {
-    if (displayScreen.value <= "0") {
-        displayScreen.value = "";
-        displayScreen.value += "5"
-        displayValue += "5"
-    } else {
-        displayScreen.value += "5"
-        displayValue += "5"
-    }
+    numberBtn("5");
 })
 
 // Six button
 let sixBtnPress = document.querySelector("#sixBtn");
 sixBtnPress.addEventListener('click', () => {
-    if (displayScreen.value <= "0") {
-        displayScreen.value = "";
-        displayScreen.value += "6"
-        displayValue += "6"
-    } else {
-        displayScreen.value += "6"
-        displayValue += "6"
-    }
+    numberBtn("6");
 })
 
 // Seven button
 let sevenBtnPress = document.querySelector("#sevenBtn");
 sevenBtnPress.addEventListener('click', () => {
-    if (displayScreen.value <= "0") {
-        displayScreen.value = "";
-        displayScreen.value += "7"
-        displayValue += "7"
-    } else {
-        displayScreen.value += "7"
-        displayValue += "7"
-    }
+    numberBtn("7");
 })
 
 // Eight button
 let eightBtnPress = document.querySelector("#eightBtn");
 eightBtnPress.addEventListener('click', () => {
-    if (displayScreen.value <= "0") {
-        displayScreen.value = "";
-        displayScreen.value += "8"
-        displayValue += "8"
-    } else {
-        displayScreen.value += "8"
-        displayValue += "8"
-    }
+    numberBtn("8");
 })
 
 // Nine button
 let nineBtnPress = document.querySelector("#nineBtn");
 nineBtnPress.addEventListener('click', () => {
-    if (displayScreen.value <= "0") {
-        displayScreen.value = "";
-        displayScreen.value += "9"
-        displayValue += "9"
-    } else {
-        displayScreen.value += "9"
-        displayValue += "9"
-    }
+    numberBtn("9");
 })
 
 // Clear button
 let clearBtn = document.querySelector('#clearBtn');
 clearBtn.addEventListener('click', () => {
     chosenOperator = "";
-    firstValue = "";
     secondValue = "";
     displayValue = "";
-    displayScreen.value = "0";
+    displayScreen.value = "";
     totalValue = 0;
+    clearFlag = false;
+    clearFlagSecond = false;
 })
 
 // Add button
 let addBtn = document.querySelector("#addBtn");
 addBtn.addEventListener('click', () => {
-    chosenOperator = "add";
-    firstValue = displayValue
-    displayValue = "";
-    displayScreen.value = "0"
+    // if (totalValue === 0) {
+    //     chosenOperator = "add";
+    //     totalValue = displayValue;
+    //     displayValue = "";
+    //     displayScreen.value = "0"
+    // } else if (secondValue === "") {
+    //     operateResults();
+    // } else {
+    //     chosenOperator = "add";
+    //     operateResults();
+    // }
+    operatorBtn("add");
 })
 
 // Subtract button
 let subtractBtn = document.querySelector("#subtractBtn");
 subtractBtn.addEventListener('click', () => {
-    chosenOperator = "subtract";
-    firstValue = displayValue
-    displayValue = "";
-    displayScreen.value = "0"
+    operatorBtn("subtract");
 })
 
 // Multiply button
 let multiplyBtn = document.querySelector("#multiplyBtn");
 multiplyBtn.addEventListener('click', () => {
-    chosenOperator = "multiply";
-    firstValue = displayValue
-    displayValue = "";
-    displayScreen.value = "0"
+    operatorBtn("multiply");
 })
 
 // Divide button
 let divideBtn = document.querySelector("#divideBtn");
 divideBtn.addEventListener('click', () => {
-    chosenOperator = "divide";
-    firstValue = displayValue
-    displayValue = "";
-    displayScreen.value = "0"
+    operatorBtn("divide");
 })
 
 // Equal button
 let equalBtn = document.querySelector('#equalBtn');
 equalBtn.addEventListener('click', () => {
+    // secondValue = displayValue;
+    // totalValue = parseInt(totalValue);
+    // secondValue = parseInt(secondValue);
+    // operate(chosenOperator, totalValue, secondValue);
+    // displayValue = totalValue;
+    // displayScreen.value = totalValue;
+    // clearFlag = true;
+    operateResults();
+})
+
+// Function to generate a total and show it on the display
+function operateResults() {
     secondValue = displayValue;
-    firstValue = parseInt(firstValue);
+    // totalValue = parseInt(totalValue);
     secondValue = parseInt(secondValue);
-    operate(chosenOperator, firstValue, secondValue);
+    operate(chosenOperator, totalValue, secondValue);
     displayValue = totalValue;
     displayScreen.value = totalValue;
-})
+    clearFlag = true;
+}
+
+// Function for the number buttons
+function numberBtn(value) {
+    if (totalValue === 0 && displayValue != "0") {
+        // displayScreen.value = "";
+        displayScreen.value += `${value}`
+        displayValue += `${value}`
+    } else if (totalValue === 0 && displayScreen.value === "0") {
+        displayScreen.value = `${value}`
+        displayValue = `${value}`
+    } else if (clearFlag === true) {
+        displayScreen.value = ""
+        clearFlag = false;
+        displayScreen.value = `${value}`
+        displayValue = `${value}`
+        chosenOperator = ""
+    } else {
+        if (displayValue.value != "" && clearFlagSecond === false) {
+            displayScreen.value = `${value}`
+            displayValue = `${value}`
+            clearFlagSecond = true;
+        } else {
+            displayScreen.value += `${value}`
+            displayValue += `${value}`
+        }
+    }
+}
+
+function operatorBtn(operator) {
+    if (totalValue === 0) {
+        chosenOperator = `${operator}`;
+        totalValue = displayValue;
+        totalValue = parseInt(totalValue);
+        // displayValue = "";
+        // displayScreen.value = "0"
+    } else if (secondValue === "") {
+        operateResults();
+    } else {
+        chosenOperator = `${operator}`;
+        operateResults();
+    }
+}
